f50f8d71ae4923e8c0950b104af50b3001b6cf49
Ubuntu <ubuntu@ip-172-16-240-104.ec2.internal>
2018-11-11 22:54:38 +0000

    path to reference is now passed in as command line argument

--- build_upset_input.py
+++ build_upset_input.py
@@ -4,9 +4,9 @@ import os
 import gzip
 import sys
 
-true_variants = "/home/ubuntu/data/giab-SV/HG002_SVs_Tier1_v0.6.vcf.gz"
 #path_to_vcf_files = "/home/ubuntu/data/giab-SV/hs37d5"
 path_to_vcf_files = sys.argv[1]
+true_variants = sys.argv[2]
 vcf_files = glob(path_to_vcf_files + "/giab*.txt")
 vcf_files = ["/home/ubuntu/data/giab-SV/hs37d5/tp-base.vcf", "/home/ubuntu/data/giab-SV/hs37d5/test.vcf"]
 
